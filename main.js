e076551b64383b344e593b422d218de85f57ba53
DavidVer98 <davidcuevas15@hotmail.com>
2022-07-11 18:24:15 -0400

    creacion de la app Vue

---
+++ main.js
@@ -0,0 +1,10 @@
+const app = Vue.createApp({
+    data: function(){
+        return{
+            product : 'Socks',
+            description: 'Size G and M, cotton'
+        }
+    }
+})
+
+//app.mount("#app");
