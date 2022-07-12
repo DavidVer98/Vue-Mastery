08ef47f2829aec0527c0084753d38f2ee2007175
DavidVer98 <davidcuevas15@hotmail.com>
2022-07-12 17:10:37 -0400

    Attribute Blinding

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
     data: function(){
         return{
             product : 'Socks',
-            description: 'Size G and M, cotton'
+            description: 'Size G and M, cotton',
+            image: './assets/images/socks_blue.jpg'
         }
     }
 })
