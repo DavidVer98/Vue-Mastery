101f410156b8a00570be2f29a81e9ed4be3ea05b
DavidVer98 <davidcuevas15@hotmail.com>
2022-07-12 19:13:39 -0400

    List Rendering

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const app = Vue.createApp({
     data: function(){
         return{
             product : 'Socks',
-            description: 'Size G and M, cotton',
             image: './assets/images/socks_blue.jpg',
-            inventory: 100
+            inventory: 100,
+            details: ['50% cotton','30% wool','20% polyester'],
+            variants : [
+                { id: 2234, color:'green'},
+                { id: 2235, color:'blue'}
+            ],
+            sizes:['M','G','P']
         }
     }
 })
