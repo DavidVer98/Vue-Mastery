90cb8d66f7e59d12b062d60d207735f24157cc2c
DavidVer98 <davidcuevas15@hotmail.com>
2022-07-12 18:34:08 -0400

    Conditional Rendering

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
         return{
             product : 'Socks',
             description: 'Size G and M, cotton',
-            image: './assets/images/socks_blue.jpg'
+            image: './assets/images/socks_blue.jpg',
+            inventory: 100
         }
     }
 })
