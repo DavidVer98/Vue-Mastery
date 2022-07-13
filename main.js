192c036b48c7cb1e7ac129310581049dcf83b283
DavidVer98 <davidcuevas15@hotmail.com>
2022-07-12 21:39:34 -0400

    Event Handling

--- main.js
+++ main.js
@@ -6,12 +6,26 @@ const app = Vue.createApp({
             inventory: 100,
             details: ['50% cotton','30% wool','20% polyester'],
             variants : [
-                { id: 2234, color:'green'},
-                { id: 2235, color:'blue'}
+                { id: 2234, color:'green', image:'./assets/images/socks_green.jpg'},
+                { id: 2235, color:'blue', image:'./assets/images/socks_blue.jpg'}
             ],
-            sizes:['M','G','P']
+            sizes:['M','G','P'],
+            cart:0
         }
-    }
+    },
+    methods:{
+        updateImage(variantImage){
+            this.image = variantImage
+        },
+        addToCart(){
+            this.cart +=1
+        },
+        deleteToCart(){
+            if(this.cart > 0)
+                this.cart -=1
+        }
+    },
 })
-
+app.config.performance = true;
+app.config.devtools = true;
 //app.mount("#app");
