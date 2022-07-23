140a6a35e9a392dd80fe109be6490ab995e3cdb3
DavidVer98 <davidcuevas15@hotmail.com>
2022-07-22 22:12:58 -0400

    Vue Router Essentials

--- Real World Vue 3/real-worl-vue/src/router/index.js
+++ Real World Vue 3/real-worl-vue/src/router/index.js
@@ -1,11 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import EventListView from "../views/EventList.vue";
+import About from "../views/AboutView.vue";
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
+    name: "EventList",
+    component: EventListView,
   },
   {
     path: "/about",
@@ -13,8 +14,7 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    component: About,
   },
 ];
 
--- Real World Vue 3/real-worl-vue/src/views/EventList.vue
+++ Real World Vue 3/real-worl-vue/src/views/EventList.vue
@@ -12,7 +12,7 @@
 import EventCard from "@/components/EventCard.vue";
 
 export default {
-  name: "HomeView",
+  name: "EventList",
   components: {
     EventCard,
   },
