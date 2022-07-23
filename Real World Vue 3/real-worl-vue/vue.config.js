439b6bb44e1a1f4d589662abb9d17c463d241be0
DavidVer98 <davidcuevas15@hotmail.com>
2022-07-22 21:46:56 -0400

    Single File Components

--- Real World Vue 3/real-worl-vue/vue.config.js
+++ Real World Vue 3/real-worl-vue/vue.config.js
@@ -1,4 +1,8 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true,
+  transpileDependencies: false,
+  publicPath: "./",
+  chainWebpack: config => {
+    config.module.rules.delete('eslint');
+}
 });
